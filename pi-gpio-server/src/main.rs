017b688b62791deaa252bb2ae9c2c9351400a413
ciwolsey <carl@wolsey.org>
2023-03-06 14:54:05 +0000

    Improve naming

--- pi-gpio-server/src/main.rs
+++ pi-gpio-server/src/main.rs
@@ -6,12 +6,13 @@ use gpio::{GpioIn, GpioOut};
 
 #[tokio::main]
 async fn main() {
-    let mut gpio23 = gpio::sysfs::SysFsGpioInput::open(23).unwrap();
-	let mut gpio17 = gpio::sysfs::SysFsGpioInput::open(17).unwrap();
+    let mut alarm = gpio::sysfs::SysFsGpioInput::open(23).unwrap();
+	let mut rain = gpio::sysfs::SysFsGpioInput::open(17).unwrap();
 
 	loop {
-		println!("GPIO17: {:?}", gpio17.read_value().unwrap());
-		println!("GPIO23: {:?}", gpio23.read_value().unwrap());
+		println!("Rain: {:?}", rain.read_value().unwrap());
+		println!("Alarm: {:?}", alarm.read_value().unwrap());
+
 		thread::sleep(time::Duration::from_millis(100));
 	} 
 }
