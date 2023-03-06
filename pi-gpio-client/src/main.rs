4e8e7cdd0d50af8e27e65b0e517ee84887d870e8
ciwolsey <carl@wolsey.org>
2023-03-06 17:25:33 +0000

    Test buzzer

--- pi-gpio-client/src/main.rs
+++ pi-gpio-client/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use core::time;
+use gpio::{sysfs::SysFsGpioInput, GpioIn, GpioOut};
+use multicaster::{self, Multicaster};
+use std::{thread, time::Duration};
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    //let multi = Multicaster::new("0.0.0.0", "239.0.0.20", 5007, true).await;
+    let mut buzzer = gpio::sysfs::SysFsGpioOutput::open(27).unwrap();
+
+    buzzer.set_high().unwrap();
+    thread::sleep(Duration::from(time::Duration::from_millis(500)));
+    buzzer.set_low().unwrap();
 }
