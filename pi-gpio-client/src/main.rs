4a59bcc6b826c64586e0e29078949c5808ec0342
ciwolsey <carl@wolsey.org>
2023-03-06 17:56:48 +0000

    Add buzzer struct

--- pi-gpio-client/src/main.rs
+++ pi-gpio-client/src/main.rs
@@ -1,15 +1,32 @@
 use core::time;
-use gpio::{sysfs::SysFsGpioInput, GpioIn, GpioOut};
+use gpio::{sysfs::{SysFsGpioInput, SysFsGpioOutput}, GpioIn, GpioOut};
 use multicaster::{self, Multicaster};
 use std::{thread, time::Duration};
 use tokio;
 
+
+
+struct Buzzer {
+    gpio: SysFsGpioOutput
+}
+
+impl Buzzer {
+    fn new() -> Self {
+        Self {
+            gpio: gpio::sysfs::SysFsGpioOutput::open(27).unwrap()
+        }
+    }
+    fn beep(&mut self, duration: u64) {
+        self.gpio.set_high().unwrap();
+        thread::sleep(Duration::from(time::Duration::from_millis(500)));
+        self.gpio.set_low().unwrap();
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    //let multi = Multicaster::new("0.0.0.0", "239.0.0.20", 5007, true).await;
-    let mut buzzer = gpio::sysfs::SysFsGpioOutput::open(27).unwrap();
-
-    buzzer.set_high().unwrap();
-    thread::sleep(Duration::from(time::Duration::from_millis(500)));
-    buzzer.set_low().unwrap();
+    let multi = Multicaster::new("0.0.0.0", "239.0.0.20", 5007, true).await;
+    
+    let mut buzzer = Buzzer::new();
+    buzzer.beep(500);
 }
