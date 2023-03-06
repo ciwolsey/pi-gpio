24716efeff3eff391e03663d1bc09edee400761c
ciwolsey <carl@wolsey.org>
2023-03-06 15:48:14 +0000

    Implement basic pin structure

--- pi-gpio-server/src/main.rs
+++ pi-gpio-server/src/main.rs
@@ -1,18 +1,76 @@
 use core::time;
+use gpio::{sysfs::SysFsGpioInput, GpioIn, GpioOut};
+use multicaster;
 use std::thread;
 use tokio;
-use multicaster;
-use gpio::{GpioIn, GpioOut};
+
+struct Pins {
+    alarm: SysFsGpioInput,
+    alarm_last: bool,
+    rain: SysFsGpioInput,
+    rain_last: bool,
+}
+
+impl Pins {
+    fn new() -> Self {
+        let mut rain = gpio::sysfs::SysFsGpioInput::open(17).unwrap();
+        let mut alarm = gpio::sysfs::SysFsGpioInput::open(23).unwrap();
+        let rain_val = rain.read_value().unwrap().into();
+        let alarm_val = alarm.read_value().unwrap().into();
+
+        Self {
+            alarm,
+            rain,
+            alarm_last: alarm_val,
+            rain_last: rain_val,
+        }
+    }
+
+    fn poll(&mut self, pin: Pin) -> Option<bool> {
+        match pin {
+            Pin::Rain => {
+                let current: bool = self.rain.read_value().unwrap().into();
+                let changed = current == self.rain_last;
+                self.rain_last = current;
+                if changed {
+                    Some(current)
+                } else {
+                    None
+                }
+            }
+            Pin::Alarm => {
+                let current: bool = self.alarm.read_value().unwrap().into();
+                let changed = current == self.alarm_last;
+                self.alarm_last = current;
+                if changed {
+                    Some(current)
+                } else {
+                    None
+                }
+            }
+        }
+    }
+}
+
+enum Pin {
+    Rain,
+    Alarm,
+}
 
 #[tokio::main]
 async fn main() {
-    let mut alarm = gpio::sysfs::SysFsGpioInput::open(23).unwrap();
-	let mut rain = gpio::sysfs::SysFsGpioInput::open(17).unwrap();
+    let mut pins = Pins::new();
 
-	loop {
-		println!("Rain: {:?}", rain.read_value().unwrap());
-		println!("Alarm: {:?}", alarm.read_value().unwrap());
+    let rain = pins.poll(Pin::Rain);
+    let alarm = pins.poll(Pin::Alarm);
 
-		thread::sleep(time::Duration::from_millis(100));
-	} 
+    match rain {
+        Some(change_to) => println!("Rain value changed to: {}", change_to),
+        None => (),
+    }
+
+    match alarm {
+        Some(change_to) => println!("Rain value changed to: {}", change_to),
+        None => (),
+    }
 }
